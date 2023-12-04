8bf91c5ad733d255209dd93c908c2273777afe22
JYOSNA PHILIP <142787944+jyosnaphilip@users.noreply.github.com>
2023-12-05 01:21:02 +0530

    only form validation, navbar bug left to resolve

--- portfolio.js
+++ portfolio.js
@@ -17,37 +17,8 @@ function tick_mark(){
   document.getElementById("submit").innerHTML="<i class='fa-solid fa-circle-check' style='color: #9bfda2;'></i>";
 }
 
+function scroll_header(){
+  window.scrollTo(0,300);
+}
 
-
-
-
-// // When the user scrolls down 50px from the top of the document, resize the header's font size
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-   
-//     document.getElementById("name_pic").style.width= "180px";
-//     document.getElementById("name_pic").style.height= "80px";
-//     document.getElementById("pic").style.display= "none";
-//     document.getElementById("designatn").style.display= "none";
-//     document.getElementById("name_pic").style.position="sticky";
-
-
-//   } 
-//   else {
-   
-//     document.getElementById("name_pic").style.width= "280px";
-//     document.getElementById("name_pic").style.height= "180px";
-//     document.getElementById("name_pic").style.display= "block";
-//     document.getElementById("name_pic").style.margin= "auto";
-    
-//     document.getElementById("pic").style.display= "block";
-//     document.getElementById("designatn").style.display= "inline";
-
-
-
-
-
-//
 
